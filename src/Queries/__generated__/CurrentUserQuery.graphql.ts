57c09175d098da3385b5ec36602123b0ef4a0730
Kurlex <medazizbelhajamor@gmail.com>
2024-06-21 19:22:37 +0100

    run relay

---
+++ src/Queries/__generated__/CurrentUserQuery.graphql.ts
@@ -0,0 +1,94 @@
+/**
+ * @generated SignedSource<<1d31d1c751391cccc993c5ebfcd10695>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type CurrentUserQuery$variables = Record<PropertyKey, never>;
+export type CurrentUserQuery$data = {
+  readonly currentUser: {
+    readonly name: string | null | undefined;
+  } | null | undefined;
+};
+export type CurrentUserQuery = {
+  response: CurrentUserQuery$data;
+  variables: CurrentUserQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "CurrentUserQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "currentUser",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "CurrentUserQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "currentUser",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "61594faf30230cc9cc74256af1aed10c",
+    "id": null,
+    "metadata": {},
+    "name": "CurrentUserQuery",
+    "operationKind": "query",
+    "text": "query CurrentUserQuery {\n  currentUser {\n    name\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "3c0b9ca7f11f483986dd11c8058f72ab";
+
+export default node;
